fb5ae03390c103bffe808c3e68c050dfac0e8d17
Prashant <prashantsherkar7515400@gmail.com>
2025-07-28 14:03:37 +0530

    issue resolved

--- services/llm.js
+++ services/llm.js
@@ -1,9 +1,11 @@
 
 import { OpenAI } from "openai"; 
+import dotenv from "dotenv";
+dotenv.config();
+
 const openai = new OpenAI({
   apiKey:
-    process.env.OPENAI_API_KEY ||
-    "",
+    process.env.OPENAI_API_KEY,
 });
 
 export const generateSlug = async (title, content) => {
